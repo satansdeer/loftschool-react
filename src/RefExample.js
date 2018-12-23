11ba66178f6778cf3b123be8c5306a2b31cdd2b0
Maksim Ivanov <satansdeer@gmail.com>
2018-12-23 21:00:18 +0100

    Set RefExample to initial state

--- src/RefExample.js
+++ src/RefExample.js
@@ -4,22 +4,10 @@ const videoSource =
   'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4';
 
 export class RefExample extends Component {
-  video = React.createRef();
-
-  playVideo = () => {
-    this.video.current.play();
-  };
-
-  stopVideo = () => {
-    this.video.current.stop();
-  };
-
   render() {
     return (
       <div>
-        <button onClick={this.playVideo}>Play</button>
-        <button onClick={this.stopVideo}>Stop</button>
-        <video width="320" height="240" ref={this.video}>
+        <video width="320" height="240">
           <source src={videoSource} type="video/mp4" />
         </video>
       </div>
