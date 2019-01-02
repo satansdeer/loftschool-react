128075296fcb6ac20e67a89f4d1cdeb6062a1365
Maksim Ivanov <satansdeer@gmail.com>
2019-01-02 09:43:21 +0300

    Rewrite reducer using handleActions

--- src/modules/series/reducer.js
+++ src/modules/series/reducer.js
@@ -3,37 +3,70 @@ import {
   getSeriesSuccess,
   getSeriesFailure,
 } from './actions';
+import { handleActions } from 'redux-actions';
+import { combineReducers } from 'redux';
 
-const initialState = {
-  series: [],
-  isLoading: false,
-  error: null,
-};
+const series = handleActions(
+  {
+    [getSeriesRequest]: () => [],
+    [getSeriesSuccess]: (_state, action) => action.payload,
+  },
+  [],
+);
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case getSeriesRequest.toString():
-      return {
-        ...state,
-        series: [],
-        isLoading: true,
-      };
+const isLoading = handleActions(
+  {
+    [getSeriesRequest]: () => true,
+    [getSeriesSuccess]: () => false,
+    [getSeriesFailure]: () => false,
+  },
+  false,
+);
 
-    case getSeriesSuccess.toString():
-      return {
-        ...state,
-        series: action.payload,
-        isLoading: false,
-      };
+const error = handleActions(
+  {
+    [getSeriesRequest]: () => null,
+    [getSeriesFailure]: (_state, action) => action.payload,
+  },
+  null,
+);
 
-    case getSeriesFailure.toString():
-      return {
-        ...state,
-        error: action.payload,
-        isLoading: false,
-      };
+export default combineReducers({
+  series,
+  isLoading,
+  error,
+});
 
-    default:
-      return state;
-  }
-};
+// const initialState = {
+//   series: [],
+//   isLoading: false,
+//   error: null,
+// };
+
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case getSeriesRequest.toString():
+//       return {
+//         ...state,
+//         series: [],
+//         isLoading: true,
+//       };
+
+//     case getSeriesSuccess.toString():
+//       return {
+//         ...state,
+//         series: action.payload,
+//         isLoading: false,
+//       };
+
+//     case getSeriesFailure.toString():
+//       return {
+//         ...state,
+//         error: action.payload,
+//         isLoading: false,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
