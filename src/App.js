1f634a8adcba91764558377ff20c9df3d9546df2
Maksim Ivanov <satansdeer@gmail.com>
2019-01-18 06:37:39 +0100

    Add final form example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from "react";
-import { reduxForm, Field } from "redux-form";
+import React, { Component } from "react";
+import { Form, Field } from "react-final-form";
 import "./App.css";
 
-const customField = ({
-  input,
-  type,
-  placeholder,
-  id,
-  meta: { touched, error },
-  ...rest
-}) => {
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const Input = ({ input, meta, label }) => {
   return (
-    <div>
-      <input {...input} placeholder={placeholder} type={type} id={id} />
-      {touched && error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+    <>
+      <p>{label}</p>
+      <input {...input} />
+      {meta.error && meta.visited && !meta.active && (
+        <pre style={{ color: "red" }}>{meta.error}</pre>
+      )}
+    </>
   );
 };
 
-const myValidator = values => {
+const onSubmit = () => {
+  console.log("Submit");
+};
+
+const validate = values => {
   const errors = {};
   if (!values.firstName) {
     errors.firstName = "First name is required";
@@ -26,38 +28,39 @@ const myValidator = values => {
   return errors;
 };
 
-const WrappedForm = ({ handleSubmit }) => {
-  return (
-    <form onSubmit={handleSubmit(val => console.log(val))}>
-      <div>
-        <Field
-          name="firstName"
-          type="text"
-          id="first-name"
-          placeholder="Maksim"
-          component={customField}
-        />
-        <Field
-          name="lastName"
-          type="text"
-          id="last-name"
-          placeholder="Ivanov"
-          component={customField}
-        />
-        <Field
-          name="email"
-          type="email"
-          id="email"
-          placeholder="maksim@ivanov.com"
-          component={customField}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
+const validateLastName = async value => {
+  if (!value) {
+    return "Last name is required";
+  }
+
+  await sleep(500);
+
+  if (value === "test") {
+    return "Last name taken";
+  }
 };
 
-export default reduxForm({
-  form: "wrappedForm",
-  validate: myValidator
-})(WrappedForm);
+class App extends Component {
+  render() {
+    return (
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form onSubmit={handleSubmit}>
+            <Field name="firstName" label="First name" component={Input} />
+            <Field
+              name="lastName"
+              label="Last name"
+              component={Input}
+              validate={validateLastName}
+            />
+            <Field name="email" label="Email" component={Input} />
+          </form>
+        )}
+      />
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import App from "./App";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const rootReducer = combineReducers({
-  form: formReducer
-});
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
